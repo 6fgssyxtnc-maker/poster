be1b79e3427607089679d5f967fc55266199f3a3
6fgssyxtnc-maker <6fgssyxtnc@privaterelay.appleid.com>
2026-02-20 07:58:27 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Rnd } from "react-rnd";
 
 export default function App() {
@@ -6,8 +6,6 @@ export default function App() {
   const [imageObj, setImageObj] = useState(null);
   const [position, setPosition] = useState({ x: 240, y: 240 });
   const [size, setSize] = useState({ width: 600, height: 600 });
-
-  const posterRef = useRef(null);
 
   const handleUpload = (file) => {
     const url = URL.createObjectURL(file);
@@ -30,29 +28,15 @@ export default function App() {
 
     poster.onload = () => {
       if (imageObj) {
-      const imgRatio = imageObj.width / imageObj.height;
-const boxRatio = size.width / size.height;
+        const scaleFactor = 2; // because preview is scaled 0.5
 
-let drawWidth = size.width;
-let drawHeight = size.height;
-
-if (imgRatio > boxRatio) {
-  // image is wider
-  drawHeight = size.width / imgRatio;
-} else {
-  // image is taller
-  drawWidth = size.height * imgRatio;
-}
-
-const scaleFactor = 2; // because preview is 0.5 scale
-
-ctx.drawImage(
-  imageObj,
-  position.x * scaleFactor,
-  position.y * scaleFactor,
-  size.width * scaleFactor,
-  size.height * scaleFactor
-);
+        ctx.drawImage(
+          imageObj,
+          position.x * scaleFactor,
+          position.y * scaleFactor,
+          size.width * scaleFactor,
+          size.height * scaleFactor
+        );
       }
 
       ctx.drawImage(poster, 0, 0, 1080, 1080);
@@ -75,73 +59,73 @@ ctx.drawImage(
       />
 
       <br /><br />
-<button onClick={downloadPoster}>
+
+      <button onClick={downloadPoster}>
         Download PNG
       </button>
+
+      <br /><br />
+
+      {/* Preview wrapper */}
+      <div
+        style={{
+          width: 540,
+          height: 540,
+          overflow: "hidden"
+        }}
+      >
+        <div
+          style={{
+            width: 1080,
+            height: 1080,
+            position: "relative",
+            transform: "scale(0.5)",
+            transformOrigin: "top left"
+          }}
+        >
+          {userImage && (
+            <Rnd
+              size={size}
+              position={position}
+              bounds="parent"
+              lockAspectRatio={true}
+              onDragStop={(e, d) =>
+                setPosition({ x: d.x, y: d.y })
+              }
+              onResizeStop={(e, dir, ref, delta, pos) => {
+                setSize({
+                  width: parseInt(ref.style.width),
+                  height: parseInt(ref.style.height)
+                });
+                setPosition(pos);
+              }}
+            >
+              <img
+                src={userImage}
+                alt="user"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover"
+                }}
+              />
+            </Rnd>
+          )}
+
+          <img
+            src="/poster-bg.png"
+            alt="poster"
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: 0,
+              left: 0,
+              pointerEvents: "none"
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
-  
-    <div
-  style={{
-    width: 540,
-    height: 540,
-    overflow: "hidden"
-  }}
->
-  <div
-    style={{
-      width: 1080,
-      height: 1080,
-      position: "relative",
-      transform: "scale(0.5)",
-      transformOrigin: "top left"
-    }}
-  >
-      >
-        {userImage && (
-          <Rnd
-            size={size}
-            position={position}
-            bounds="parent"
-            lockAspectRatio={true}
-            onDragStop={(e, d) =>
-              setPosition({ x: d.x, y: d.y })
-            }
-            onResizeStop={(e, dir, ref, delta, pos) => {
-              setSize({
-                width: parseInt(ref.style.width),
-                height: parseInt(ref.style.height)
-              });
-              setPosition(pos);
-            }}
-          >
-            <img
-              src={userImage}
-              alt="user"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover"
-              }}
-            />
-          </Rnd>
-        )}
-
-        <img
-          src="/poster-bg.png"
-          alt="poster"
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100%",
-            top: 0,
-            left: 0,
-            pointerEvents: "none"
-          }}
-        />
-      </div>
-
-      <br />
-
-      
 }
